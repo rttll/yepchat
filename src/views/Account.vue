2cf7093b711fb74a06bca76bd10f41de856e9c1e
Adro <acousens@gmail.com>
2020-06-21 14:05:20 -0700

    Add form save feedback

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="bg-gray-100 flex items-center md:items-baseline justify-center h-screen p-4 md:py-0">
-    <div class="fixed top-0 left-0 p-4 pt-2" v-if="!newAccount">
-      <router-link to="/" class="text-lg"> &larr; Chat </router-link>
+    <div class="fixed top-0 left-0 p-4 pt-2" v-if="name">
+      <transition name="fade">
+        <router-link to="/" class="text-lg"> 
+          <span class="wigwag inline-block" :class="{active : saved}"> &larr; </span>
+          Chat 
+        </router-link>
+      </transition>
     </div>
     <div class="bg-white w-full max-w-md mx-auto shadow-xl md:mt-24 relative rounded">
       <div class="w-full w-full flex items-center justify-center relative -mt-16">
@@ -12,7 +17,7 @@
 
       <div class="px-8 my-10">
         <label for="name" class="block shadow-md">
-          <input v-model="name" type="text" id="name" class="border p-4 block w-full rounded" placeholder="Username" autocomplete="off">
+          <input @input="input" v-model="name" type="text" id="name" class="border p-4 block w-full rounded" placeholder="Username" autocomplete="off">
         </label>
 
         <div class="mt-4">
@@ -22,7 +27,7 @@
               :class="{'bg-green-200' : avatar === 'fox', 'hover:bg-green-100' : avatar !== 'fox'}" >
               <input 
                 type="radio"
-                v-model="avatar" 
+                @input="input" v-model="avatar" 
                 id="fox" 
                 value="fox"
                 class="opacity-0 absolute"
@@ -33,7 +38,7 @@
               :class="{'bg-green-200' : avatar === 'bear', 'hover:bg-green-100' : avatar !== 'bear'}" >
               <input 
                 type="radio"
-                v-model="avatar" 
+                @input="input" v-model="avatar" 
                 id="bear" 
                 value="bear"
                 class="opacity-0 absolute"
@@ -44,7 +49,7 @@
               :class="{'bg-green-200' : avatar === 'hedgehog', 'hover:bg-green-100' : avatar !== 'hedgehog'}" >
               <input 
                 type="radio"
-                v-model="avatar" 
+                @input="input" v-model="avatar" 
                 id="hedgehog" 
                 value="hedgehog"
                 class="opacity-0 absolute"
@@ -55,7 +60,7 @@
               :class="{'bg-green-200' : avatar === 'owl', 'hover:bg-green-100' : avatar !== 'owl'}" >
               <input 
                 type="radio"
-                v-model="avatar" 
+                @input="input" v-model="avatar" 
                 id="owl" 
                 value="owl"
                 class="opacity-0 absolute"
@@ -66,12 +71,20 @@
         </div>
 
       </div>
-      <div class="bg-gray-100 py-4 px-8 flex justify-end">
+      <div class="bg-gray-100 py-4 px-8 flex justify-end relative">
         <button 
           v-on:click="save"
           class="button bg-white border shadow-md hover:shadow-sm">
           Save
         </button>
+        <div class="absolute left-0 right-0 px-8" v-if="saved">
+          <transition name="fade">
+            <div class="bg-green-200 border border-green-600 text-green-500 block rounded p-4 flex items-center justify-between">
+              <p class="">Saved!</p>
+              <span class="cursor-pointer" v-on:click="saved = false"> &times; </span>
+            </div>
+          </transition>
+        </div>
       </div>
       <div v-if="!newAccount" class="relative">
         <span v-on:click="destroy" class="absolute cursor-pointer text-xs p-1 hover:text-red-500">Destroy</span>
@@ -92,6 +105,7 @@
       return {
         heading: "Pick your info",
         newAccount: localStorage.getItem('yepchat') === null,
+        saved: false,
         name: null,
         avatar: null,
         avatars: {
@@ -109,6 +123,9 @@
       }
     },
     methods: {
+      input: function() {
+        this.saved = false
+      },
       save: async function() {
         if (this.name == null || this.avatar === null) return false
         var data = {
@@ -121,6 +138,7 @@
             Store.updateUser(data.name)
             Store.updateAvatar(data.avatar)
           }
+          this.saved = true
         } catch (error) {
           console.log(error)
         }
@@ -163,4 +181,23 @@
   input:focus, input:active, .button:focus, .button:active {
     @apply shadow-outline
   }
+
+  .wigwag {
+    position: relative;
+  }
+  .wigwag.active {
+    animation: pulse 1s infinite alternate;
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: translateX(-5px);
+      /* color: blue; */
+    }
+    100% {
+      transform: translateX(5px);
+      /* color: red */
+    }
+  }
+
 </style>
